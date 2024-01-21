35f4c5b839e98fe7858fa1eca65e020dd3433df0
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-01-21 12:01:42 -0800

    Add payment processing functions

--- Javascript_advanced/8-payments.js
+++ Javascript_advanced/8-payments.js
@@ -1,12 +1,25 @@
+/**
+ * Processes a payment.
+ * 
+ * @param {number} amount The amount to be collected.
+ */
 function processPayment(amount) {
   console.log(`Collecting payment of ${amount}`);
 }
 
+
+/**
+ * Processes an order.
+ * 
+ * @param {number} orderId The ID of the order.
+ * @param {number} amount The amount to be processed.
+ */
 function processOrder(orderId, amount) {
   console.log(`${orderId} is being processed`);
   processPayment(amount);
   console.log(`${orderId} has been fully processed`);
 }
+
 
 console.log("Porcessing orders");
 processOrder(12321, 10.99);
