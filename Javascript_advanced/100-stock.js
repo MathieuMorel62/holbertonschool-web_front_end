e9e24d2715b20d0ff04da1c38da5754040ac8956
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-01-22 07:39:39 -0800

    Add stock management functions

--- Javascript_advanced/100-stock.js
+++ Javascript_advanced/100-stock.js
@@ -3,16 +3,30 @@ const stock = {
   iphone: 4
 };
 
+/**
+* Decrements stock and logs payment processing.
+ * @param {string} itemName - Name of the item.
+ */
 function processPayment (itemName) {
   stock[itemName] = stock[itemName] - 1;
   console.log(`Payment is being processed for ${itemName}`);
 }
 
+/**
+ * Logs out-of-stock message.
+ * @param {string} itemName - Name of the item.
+ */
 function processError (itemName) {
   console.log(`No more ${itemName} in stock`);
   console.log('Payment is not being processed');
 }
 
+/**
+ * Checks stock and calls appropriate callback.
+ * @param {string} itemName - Name of the item.
+ * @param {function} callbackPayment - Function for payment processing.
+ * @param {function} callbackError - Function for error handling.
+ */
 function processOrder (itemName, callbackPayment, callbackError) {
   console.log(`Verifying the stock of ${itemName}`);
   if (stock[itemName] > 0) {
